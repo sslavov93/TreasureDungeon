4216ef2af94c875f9b105711244f325bd91c9466
sslavov93 <sslavov93@gmail.com>
2015-06-07 20:19:16 +0300

    Created User Interface

--- UI.py
+++ UI.py
@@ -1,9 +1,7 @@
 from weapon import Weapon
 from hero import Hero
-from orc import Orc
-from fight import Fight
 from dungeon import Dungeon
-
+from random import randint
 
 helper = """load_map <mapfile.txt> - Creates new dungeon with the map in mapfile
 show_map - Displays current dungeon map
@@ -17,25 +15,44 @@ exit - Closes the program"""
 def start():
     print("Welcome. Type 'help' for available commands")
     exit = False
+    created = False
     while exit is False:
         full_command = input('--> ').split(' ')
         if full_command[0] == 'load_map':
-            dungeon = Dungeon('basic_dungeon.txt')
+            dungeon = Dungeon(full_command[1])
         elif full_command[0] == 'show_map':
             print(dungeon.print_map())
         elif full_command[0] == 'create_hero':
-            my_hero = Hero(full_command[1], full_command[2], full_command[3])
+            my_hero = Hero(
+                full_command[1], int(full_command[2]), full_command[3])
+            my_hero.weapon = Weapon('Ashbringer', 40, 0.8)
+            created = True
         elif full_command[0] == 'spawn_hero':
-            if not my_hero:
-                return 'There is nobody to spawn.'
-            dungeon.spawn(full_command[1], my_hero)
-            NPC = Orc('Thrall', 400, 1.7)
-            dungeon.spawn('NPC1', NPC)
+            if created is True:
+                dungeon.spawn(full_command[1], my_hero)
+                dungeon.spawn_npcs()
+            else:
+                return 'No created characters.'
         elif full_command[0] == 'move':
             if not dungeon.ingame[full_command[1]].is_alive():
-                return 'Your character has died. Please go away.'
+                exit = True
+                return 'Your character is dead. Game over.'
             else:
-                dungeon.move(full_command[1], full_command[2])
+                if dungeon.unlocked:
+                    exit = True
+                    return 'Game Over. You have won.'
+                print(dungeon.move_player(full_command[1], full_command[2]))
+                to_move = dungeon.get_random_npc()
+                if not dungeon.ingame[to_move].is_alive():
+                    dungeon.ingame = {
+                        i: dungeon.ingame[i] for i in dungeon.ingame if i != 0}
+                    dungeon.npcs = {i: dungeon.npcs[i]
+                                    for i in dungeon.npcs if i != 0}
+                else:
+                    print(dungeon.move_npc(to_move))
+                print(dungeon.print_map())
+        elif full_command[0] == 'heal':
+            my_hero.health = my_hero.max_health
         elif full_command[0] == 'help':
             print(helper)
         elif full_command[0] == 'exit':
@@ -43,4 +60,4 @@ def start():
         else:
             print("Invalid command. Type 'help' for available commands.")
 
-start()
+print(start())
