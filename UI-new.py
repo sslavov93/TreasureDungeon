90431fce0a0bd22c525bd6cb20cf7953b66ed262
sslavov93 <sslavov93@gmail.com>
2015-10-30 10:48:24 +0200

    Initiated creation of new UI class

---
+++ UI-new.py
@@ -0,0 +1,18 @@
+# TODO:
+# One method to handle input
+# A method for each of the commands
+# A simplified execution method
+
+# from weapon import Weapon
+# from hero import Hero
+# from dungeon import Dungeon
+# from random import randint
+
+
+class UserInterface():
+    def __init__(self):
+        # self.command_list = {"input_command": script_action}
+        pass
+
+    def get_input_command(self):
+        pass
