dbb3293a94f08afbc288933f5471c28c61a8d6cc
sslavov93 <sslavov93@gmail.com>
2015-11-09 14:06:43 +0200

    Formed __init__ method

--- map_validator.py
+++ map_validator.py
@@ -1,7 +1,27 @@
+import os
+
+
 class MapValidator():
     """Base class. Validates user-created map file"""
-    def __init__(self):
-        pass
+    def __init__(self, location):
+        self.map_contents = self.read_file(location)
+        self.map_length = 0
+        self.map_width = 0
+        self.player_spawn = False
+        self.npc_spawn = False
+        self.key_present = False
+        self.chest_present = False
+
+    def read_file(self, map_file):
+        """Loads dungeon map from specified absolute path in the harddisk
+
+        Args:
+            map_file - /full/path/to/map/file.txt"""
+        if not os.path.exists(map_file):
+            return ''
+        with open(map_file, 'r') as f:
+            contents = f.read()
+        return contents
 
     def check_player_spawn(self):
         """Checks whether only one player spawn
