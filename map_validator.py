28aa4c8c60a983e96c4707aeb3503e4196c79f47
sslavov93 <sslavov93@gmail.com>
2015-11-09 13:49:59 +0200

    Added boilerplate for custom map validation script

---
+++ map_validator.py
@@ -0,0 +1,24 @@
+class MapValidator():
+    """"""
+    def __init__(self):
+        pass
+
+    def check_only_one_player_start_point_is_present(self):
+        """"""
+        pass
+
+    def check_one_or_more_npc_start_points_are_present(self):
+        """"""
+        pass
+
+    def check_dungeon_is_valid_square(self):
+        """"""
+        pass
+
+    def check_key_is_present(self):
+        """"""
+        pass
+
+    def check_chest_is_present(self):
+        """"""
+        pass
