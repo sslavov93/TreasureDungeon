7036a772176285e7301386642b6c95c1dee5b505
sslavov93 <sslavov93@gmail.com>
2015-11-09 13:55:49 +0200

    Added docstrings

--- map_validator.py
+++ map_validator.py
@@ -1,24 +1,27 @@
 class MapValidator():
-    """"""
+    """Base class. Validates user-created map file"""
     def __init__(self):
         pass
 
-    def check_only_one_player_start_point_is_present(self):
-        """"""
+    def check_player_spawn(self):
+        """Checks whether only one player spawn
+        indicator is present on the map"""
         pass
 
-    def check_one_or_more_npc_start_points_are_present(self):
-        """"""
+    def check_npc_spawn(self):
+        """Checks whether one or more NPC spawn
+        indicators are present on the map"""
         pass
 
-    def check_dungeon_is_valid_square(self):
-        """"""
+    def check_dungeon_dimentions(self):
+        """Checks whether the dungeon is a valid square
+        (Length is equal to Width)"""
         pass
 
     def check_key_is_present(self):
-        """"""
+        """Checks whether a chest key indicator is present on the map"""
         pass
 
     def check_chest_is_present(self):
-        """"""
+        """Checks whether a chest indicator is present on the map"""
         pass
